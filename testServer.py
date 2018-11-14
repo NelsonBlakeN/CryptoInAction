39703e1ef1f6191df85e4bf67a68f65806b456b4
NelsonBlakeN <nelson.blaken@gmail.com>
2018-11-14 16:47:02 -0600

    Correct test server

--- testServer.py
+++ testServer.py
@@ -8,10 +8,9 @@ server.listen(1)
 
 while True:
     print("Waiting on connection...")
-    print("Key server: {}".format(Utilities.keyserv))
+
     conn, _ = server.accept()
     print("Connected.")
-    print("Key server: {}".format(Utilities.keyserv))
 
     # Recieve response
     message = conn.recv(4096)
