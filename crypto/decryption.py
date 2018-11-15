45e1ef5efbe65b5a7afc8046c57ded6390957f4c
NelsonBlakeN <nelson.blaken@gmail.com>
2018-11-15 13:06:22 -0600

    Improvements on encryption/decryption implementation

--- crypto/decryption.py
+++ crypto/decryption.py
@@ -3,21 +3,32 @@ class Decryption(object):
         pass
 
     # Decrpyt RSA-encrypted messages
-    def rsa(self, cipher, key):
+    # cipher: the string message to decrypt
+    # key: the key used to decrypt
+    # info: a dictionary of any values (n, etc) that is necessary to decrypt
+    def rsa(self, cipher, key={}):
         return ""
 
     # Decrypt AES-128-encrypted messages
-    def aes128(self, cipher, key):
+    # cipher: the string message to decrypt
+    # key: the key used to decrypt (could be multiple values)
+    def aes128(self, cipher, key={}):
         return ""
 
     # Decrpyt DES-encrypted messages
-    def des(self, cipher, key):
+    # cipher: the string message to decrypt
+    # key: the key used to decrypt (could be multiple values)
+    def des(self, cipher, key={}):
         return ""
 
     # Decrpyt OTP-encrypted messages
-    def one_time_pad(self, cipher, key):
+    # cipher: the string message to decrypt
+    # key: the key used to decrypt (could be multiple values)
+    def one_time_pad(self, cipher, key={}):
         return ""
 
     # Decrypt El-Gamal-encrypted messages
-    def el_gamal(self, cipher, key):
+    # cipher: the string message to decrypt
+    # key: the key used to decrypt (could be multiple values)
+    def el_gamal(self, cipher, key={}):
         return ""
--- crypto/encryption.py
+++ crypto/encryption.py
@@ -3,21 +3,31 @@ class Encryption(object):
         pass
 
     # RSA Encryption
-    def rsa(self, message, key):
+    # message: the string message to encrypt
+    # key: the key used to  (could be multiple values)
+    def rsa(self, message, key={}):
         return ""
 
     # AES-128 Encryption
-    def aes128(self, message, key):
+    # message: the string message to encrypt
+    # key: the key used to encrypt (could be multiple values)
+    def aes128(self, message, key={}):
         return ""
 
     # DES Encryption
-    def des(self, message, key):
+    # message: the string message to encrypt
+    # key: the key used to encrypt (could be multiple values)
+    def des(self, message, key={}):
         return ""
 
     # One Time Pad Encryption
-    def one_time_pad(self, message, key):
+    # message: the string message to encrypt
+    # key: the key used to encrypt (could be multiple values)
+    def one_time_pad(self, message, key={}):
         return ""
 
     # El Gamal Encryption
-    def el_gamal(self, message, key):
+    # message: the string message to encrypt
+    # key: the key used to encrypt (could be multiple values)
+    def el_gamal(self, message, key={}):
         return ""
