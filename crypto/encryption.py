57470432c2d6f9b2d39e5ec6189aa305a88f2bdd
NelsonBlakeN <nelson.blaken@gmail.com>
2018-12-06 13:29:53 -0600

    Rough implementation of DES in function

--- crypto/encryption.py
+++ crypto/encryption.py
@@ -12,29 +12,6 @@ if_perm = [57, 49, 41, 33, 25, 17, 9,  1,
            60, 52, 44, 36, 28, 20, 12, 4,
            62, 54, 46, 38, 30, 22, 14, 6]
 
-expansion = [31, 0,  1,  2,  3,  4,
-             3,  4,  5,  6,  7,  8,
-             7,  8,  9,  10, 11, 12,
-             11, 12, 13, 14, 15, 16,
-             15, 16, 17, 18, 19, 20,
-             19, 20, 21, 22, 23, 24,
-             23, 24, 25, 26, 27, 28,
-             27, 28, 29, 30, 31, 0]
-
-sbox = [[14, 4,  13, 1, 2,  15, 11, 8,  3,  10, 6,  12, 5,  9,  0, 7],
-        [0,  15, 7,  4, 14, 2,  13, 1,  10, 6,  12, 11, 9,  5,  3, 8],
-        [4,  1,  14, 8, 13, 6,  2,  11, 15, 12, 9,  7,  3,  10, 5, 0],
-        [15, 12, 8,  2, 4,  9,  1,  7,  5,  11, 3,  14, 10, 0,  6, 13]]
-
-mangler_perm = [15, 6,  19, 20,
-                28, 11, 27, 16,
-                0,  14, 22, 25,
-                4,  17, 30, 9,
-                1,  7,  23, 13,
-                31, 26, 2,  8,
-                18, 12, 29, 5,
-                21, 10, 3,  24]
-
 class Encryption(object):
     def __init__(self):
         pass
@@ -54,7 +31,39 @@ class Encryption(object):
     # message: the string message to encrypt
     # key: the key used to encrypt (could be multiple values)
     def des(self, message, key={}):
-        return ""
+        # Translate message
+        binary_msg = bin(int(message.encode('hex'), 16))[2:]
+        while len(binary_msg) % 8 != 0:
+            binary_msg = "0" + binary_msg
+
+        # Obtain key
+        key = key['k']
+
+        # Initial permutation
+        post_iperm = ""
+        for i in range(len(binary_msg)):
+            post_iperm += binary_msg[if_perm[i]]
+
+        left_block = post_iperm[:32]
+        right_block = post_iperm[32:]
+
+        post_mangler = utils.mangler(right_block, key)
+
+        print "Post mangler:", post_mangler
+
+        # XOR: Mangler out and left block
+        post_final_xor = utils.xor(left_block, post_mangler)
+
+        final_left_block = right_block
+        final_right_block = post_final_xor
+
+        # Final permutation
+        pre_fperm = final_left_block + final_right_block
+        post_fperm = [0] * len(pre_fperm)
+        for i in range(len(pre_fperm)):
+            post_fperm[if_perm[i]] = pre_fperm[i]
+
+        return ''.join(post_fperm)
 
     # One Time Pad Encryption
     # message: the string message to encrypt
